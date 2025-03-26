fc567ab5ab1e7a4e5d78a543f9b9c556b6f12bd7
hirock0 <hirockdutta0@gmail.com>
2025-03-26 08:58:39 +0600

    corner recorrect

--- src/components/home/popurlar_service_section/service_section.tsx
+++ src/components/home/popurlar_service_section/service_section.tsx
@@ -26,7 +26,7 @@ const Service_section = () => {
 
     return (
         <div className=" relative ">
-            <div className=" bg-slate-900 -rotate-45 -top-26 -left-29 absolute w-52 h-52 " style={{ clipPath: "polygon(0 0, 100% 100%, 150% 0%)" }}>
+            <div className=" max-md:hidden bg-slate-900 -rotate-45 -top-24 -left-29 absolute w-48 h-48 " style={{ clipPath: "polygon(0 0, 100% 100%, 150% 0%)" }}>
 
             </div>
 
