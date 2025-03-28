251ff2e84d6fa003d2dc8e6d909b842295e566b1
hirock0 <hirockdutta0@gmail.com>
2025-03-29 00:45:58 +0600

    corner recorrect

--- src/components/home/acheive_section/acheive_section.tsx
+++ src/components/home/acheive_section/acheive_section.tsx
@@ -16,7 +16,7 @@ const Acheive_section = () => {
 
             >
                 {/* cut_top_left */}
-                <div className=" section_corner_cut_top h-52 right-0 -top-51 absolute left-0 bg-slate-800 w-full">
+                <div style={{ background: "url(https://s3-alpha-sig.figma.com/img/6580/8079/311c027968265125a07f026a1fd271c6?Expires=1743984000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=nMjLjWQcloLPw76EdD07N9ztCobluMQPA7K6zfOs6isP5qL78jn7DxbeAg6Xb58mhxBeHXtI~YDVwRpE3q3EWlNPRbQqEmB31z9sik9YQ7BGIx8BhO14R~CbGswgyUYyaSbjBGaZgWkWawu-c2rRQrbL7QQt4XI0jScM~kaxhMZ2xKhMjApHh8AxHrPnfbs2W79w5zHU3iVRmGT37xTyhHRxJB14ALoSw72q0h9sSpJTGZ480NANgFQ56xohRSNMsILU2C6u~dZRvvn3Emmdf4wlRHUBnGogh84ct1KDHFQF~IWj~4kIxvDl36pPEH4SJOwIJwmF1RDAw~U9fcg98Q__)",}} className=" section_corner_cut_top h-52 right-0 -top-51 absolute left-0 bg-gradient-to-b w-full">
 
                 </div>
                 {/* cut_top_left */}
